0de6cb11629889f7b814f0cab788c7af8263be1f
chilee73250 <chilee73250@gmail.com>
2023-09-27 16:35:42 +0900

    프로젝트 생성

---
+++ src/main/java/com/green/hanbang/HanbangApplication.java
@@ -0,0 +1,13 @@
+package com.green.hanbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbangApplication.class, args);
+	}
+
+}
