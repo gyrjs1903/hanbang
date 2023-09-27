42ab1bf9fa13407228121c288ac83ce97879cf27
chilee73250 <chilee73250@gmail.com>
2023-09-27 17:02:45 +0900

    프로젝트 첫 생성

--- src/main/java/com/green/hanbang/IndexController.java
+++ src/main/java/com/green/hanbang/IndexController.java
@@ -1,2 +1,15 @@
-package com.green.hanbang;public class IndexController {
+package com.green.hanbang;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    // 시작페이지
+    @GetMapping("/")
+    public String main(){
+        System.out.println(":)");
+        System.out.println("환영합니다~");
+        return "index";
+    }
 }
