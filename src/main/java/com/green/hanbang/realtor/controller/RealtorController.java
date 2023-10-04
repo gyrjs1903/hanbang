079c307e41dc9955650e06e921401b0e850599a0
toamto7577 <toamto7577@gmail.com>
2023-10-04 15:35:40 +0900

    realtor layout 생성

--- src/main/java/com/green/hanbang/realtor/controller/RealtorController.java
+++ src/main/java/com/green/hanbang/realtor/controller/RealtorController.java
@@ -3,9 +3,16 @@ package com.green.hanbang.realtor.controller;
 import com.green.hanbang.realtor.service.RealtorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/realtor")
 public class RealtorController {
     private final RealtorService realtorService;
+
+    @RequestMapping("/main")
+    public String realtor(){
+        return "realtor/realtor_layout";
+    }
 }
