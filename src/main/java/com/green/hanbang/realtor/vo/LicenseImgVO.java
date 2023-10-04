a7bc652bb5810f9dd508e43cead5b27542da6587
toamto7577 <toamto7577@gmail.com>
2023-10-04 14:20:40 +0900

    realtor파일생성

---
+++ src/main/java/com/green/hanbang/realtor/vo/LicenseImgVO.java
@@ -0,0 +1,11 @@
+package com.green.hanbang.realtor.vo;
+
+import lombok.Data;
+
+@Data
+public class LicenseImgVO {
+    private String licenseCode;
+    private String originLicenseFileName;
+    private String attachedLicenseFileName;
+    private String realtorCode;
+}
---
+++ src/main/java/com/green/hanbang/realtor/vo/RealtorDetailVO.java
@@ -0,0 +1,14 @@
+package com.green.hanbang.realtor.vo;
+
+import lombok.Data;
+
+@Data
+public class RealtorDetailVO {
+    private String realtorCode;
+    private String memberId;
+    private String officeName;
+    private int identificationNum;
+    private String licenseFileName;
+    private String authority;
+    private String requestDate;
+}
