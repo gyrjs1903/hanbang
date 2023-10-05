c05ba4fd036b7912850b03a2ec99a90b2ec55e74
toamto7577 <toamto7577@gmail.com>
2023-10-05 17:44:58 +0900

    10.05 사업자등록증 파일 첨부 오류

--- src/main/java/com/green/hanbang/realtor/service/RealtorServiceImpl.java
+++ src/main/java/com/green/hanbang/realtor/service/RealtorServiceImpl.java
@@ -1,6 +1,7 @@
 package com.green.hanbang.realtor.service;
 
 import com.green.hanbang.member.vo.MemberVO;
+import com.green.hanbang.realtor.vo.RealtorDetailVO;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,16 @@ public class RealtorServiceImpl implements RealtorService{
     @Override
     public MemberVO selectRealtorMyPage(String userNo) {
         return sqlSession.selectOne("realtorMapper.selectRealtorMyPage",userNo);
+    }
+
+    @Override
+    public String selectNextRealtorCode() {
+        return sqlSession.selectOne("realtorMapper.selectNextRealtorCode");
+    }
+
+    @Override
+    public void insertRealtorDetail(RealtorDetailVO realtorDetailVO) {
+        sqlSession.insert("realtorMapper.insertLicenseImg",realtorDetailVO);
+        sqlSession.insert("realtorMapper.insertRealtorDetail",realtorDetailVO);
     }
 }
