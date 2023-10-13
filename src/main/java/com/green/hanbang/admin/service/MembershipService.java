cd1ebc5ecb175dc50270ff975fd937b08544e9ca
jyllds22 <jyllds22@gmail.com>
2023-10-13 12:51:50 +0900

    edit membership

--- src/main/java/com/green/hanbang/admin/service/MembershipService.java
+++ src/main/java/com/green/hanbang/admin/service/MembershipService.java
@@ -11,11 +11,8 @@ public interface MembershipService {
     // 맴버쉽 카테고리 조회
     public List<MemCateVO> selectCategory();
 
-    // 맴버쉽 카테고리 별 상품 조회
-    public List<MembershipVO> selectMembershipList(String memCateCode);
-
     // 맴버쉽 상품 별 세부 상품 조회
-    public List<MemItemVO> selectMembershipItemList(String membershipCode);
+    public List<MembershipVO> selectMembershipItemList(String memCateCode);
 
     // 상품 세부 정보 조회
     public MemCateVO membershipItemDetail(MemCateVO memCateVO);
