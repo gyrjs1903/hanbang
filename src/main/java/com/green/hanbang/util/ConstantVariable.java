ee0daa75b0786c0b820755b85ade5a1185f6b5d3
changgyu73 <chilee73250@gmail.com>
2023-10-19 23:35:14 +0900

    favicon logo update

--- src/main/java/com/green/hanbang/util/ConstantVariable.java
+++ src/main/java/com/green/hanbang/util/ConstantVariable.java
@@ -7,4 +7,6 @@ public class ConstantVariable {
     //매물 이미지 파일 위치
     public static final String ROOM_UPLOAD_PATH = "D:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\room\\";
     //public static final String ROOM_UPLOAD_PATH = "C:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\room\\";
+    // 회원 프로필 사진 파일 위치
+    public static final String PROFILE_UPLOAD_PATH = "D:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\member\\profileImg\\";
 }
