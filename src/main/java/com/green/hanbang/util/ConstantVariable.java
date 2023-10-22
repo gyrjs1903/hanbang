94e8e1c2f79f9c42daee58319d3cf34e627e3940
gyrjs1903 <gyrjs1903@gmail.com>
2023-10-22 17:30:41 +0900

    경로수정

--- src/main/java/com/green/hanbang/util/ConstantVariable.java
+++ src/main/java/com/green/hanbang/util/ConstantVariable.java
@@ -5,8 +5,8 @@ public class ConstantVariable {
     //파일 업로드 위치
     public static final String UPLOAD_PATH = "D:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\realtor\\";
     //매물 이미지 파일 위치
-    //public static final String ROOM_UPLOAD_PATH = "D:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\room\\";
-    public static final String ROOM_UPLOAD_PATH = "C:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\room\\";
+    public static final String ROOM_UPLOAD_PATH = "D:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\room\\";
+    //public static final String ROOM_UPLOAD_PATH = "C:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\room\\";
     // 회원 프로필 사진 파일 위치
     public static final String PROFILE_UPLOAD_PATH = "D:\\dev\\workspaceSpring\\HanBang\\src\\main\\resources\\static\\img\\member\\profileImg\\";
 }
