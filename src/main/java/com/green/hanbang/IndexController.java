c4f4b46a2dd46597e46aba9dfcded92f2c94e9ff
chilee73250 <chilee73250@gmail.com>
2023-10-27 11:26:59 +0900

    member merge

--- src/main/java/com/green/hanbang/IndexController.java
+++ src/main/java/com/green/hanbang/IndexController.java
@@ -1,20 +1,17 @@
 package com.green.hanbang;
 
-import com.green.hanbang.member.service.MemberService2;
+import com.green.hanbang.member.service.MemberService;
 import com.green.hanbang.member.vo.MemberVO;
-import com.green.hanbang.room.vo.InquiryVO;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
-    private final MemberService2 memberService2;
+    private final MemberService memberService;
     // 시작페이지
     @GetMapping("/")
     public String main(){
@@ -23,13 +20,13 @@ public class IndexController {
     //카피
     ////////////////////////////////////////////
     //자바에서 a태그 매개변수 최대한 줄여서 보내기
-    @GetMapping("/2")
-    public String main2(Model model, HttpSession session){
-        MemberVO loginInfo = (MemberVO) session.getAttribute("loginInfo");
-        if(loginInfo.getLoginType().equals("REALTOR")){
-            model.addAttribute("authorityAlarm",memberService2.selectAuthorityAlarm(loginInfo.getUserNo()));
-            model.addAttribute("realtorInquiryList",memberService2.selectInquiryAlarm(loginInfo.getUserNo()));
-        }
-        return "main/home222";
-    }
+//    @GetMapping("/")
+//    public String main(Model model, HttpSession session){
+//        MemberVO loginInfo = (MemberVO) session.getAttribute("loginInfo");
+//        if(loginInfo.getLoginType().equals("REALTOR")){
+//            model.addAttribute("authorityAlarm",memberService.selectAuthorityAlarm(loginInfo.getUserNo()));
+//            model.addAttribute("realtorInquiryList",memberService.selectInquiryAlarm(loginInfo.getUserNo()));
+//        }
+//        return "main/home";
+//    }
 }
--- src/main/java/com/green/hanbang/member/service/MemberServiceImpl.java
+++ src/main/java/com/green/hanbang/member/service/MemberServiceImpl.java
@@ -1,10 +1,14 @@
 package com.green.hanbang.member.service;
 
+import com.green.hanbang.member.vo.AlarmVO;
 import com.green.hanbang.member.vo.MemberImgVO;
 import com.green.hanbang.member.vo.MemberVO;
+import com.green.hanbang.room.vo.InquiryVO;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -66,4 +70,22 @@ public class MemberServiceImpl implements MemberService {
         return sqlSession.delete("memberMapper.deleteMember", userNo);
     }
 
+    /* ----------------------------------------------------------------------------------- */
+
+    //승인 알림 insert
+    @Override
+    public int insertAlarm(AlarmVO alarmVO) {
+        return sqlSession.insert("member2Mapper.insertAlarm",alarmVO);
+    }
+
+    //승인 알림 띄우기
+    @Override
+    public int selectAuthorityAlarm(String userNo) {
+        return sqlSession.selectOne("member2Mapper.selectAuthorityAlarm",userNo);
+    }
+
+    @Override
+    public List<InquiryVO> selectInquiryAlarm(String userNo) {
+        return sqlSession.selectList("member2Mapper.selectInquiryAlarm",userNo);
+    }
 }
