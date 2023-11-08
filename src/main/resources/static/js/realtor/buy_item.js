531fa91f1f900304a25384bc180d175ac6da5c8a
toamto7577 <toamto7577@gmail.com>
2023-11-08 17:10:57 +0900

    수정

--- src/main/resources/static/js/realtor/buy_item.js
+++ src/main/resources/static/js/realtor/buy_item.js
@@ -2,14 +2,26 @@ function onPackage(){
     document.querySelector('.generalForm').classList.remove('active');
     document.querySelector('.plusForm').classList.remove('active');
     document.querySelector('.packageForm').classList.add('active');
+
+    document.querySelector('.packageBtn').classList.add('btnActive');
+    document.querySelector('.generalBtn').classList.remove('btnActive');
+    document.querySelector('.plusBtn').classList.remove('btnActive');
 }
 function onGeneral(){
     document.querySelector('.packageForm').classList.remove('active');
     document.querySelector('.plusForm').classList.remove('active');
     document.querySelector('.generalForm').classList.add('active');
+
+    document.querySelector('.packageBtn').classList.remove('btnActive');
+    document.querySelector('.generalBtn').classList.add('btnActive');
+    document.querySelector('.plusBtn').classList.remove('btnActive');
 }
 function onPlus(){
     document.querySelector('.generalForm').classList.remove('active');
     document.querySelector('.packageForm').classList.remove('active');
     document.querySelector('.plusForm').classList.add('active');
+    
+    document.querySelector('.packageBtn').classList.remove('btnActive');
+    document.querySelector('.generalBtn').classList.remove('btnActive');
+    document.querySelector('.plusBtn').classList.add('btnActive');
 }
