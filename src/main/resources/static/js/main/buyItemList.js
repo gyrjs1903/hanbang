136cc792dcf30a69737614ffe18158f5b0f0994f
jyllds22 <jyllds22@gmail.com>
2023-11-09 17:44:14 +0900

    amdin css complete

--- src/main/resources/static/js/main/buyItemList.js
+++ src/main/resources/static/js/main/buyItemList.js
@@ -1,1 +1,20 @@
-// 구매하기 클릭시 alert 창 띄우기
+// itemContent + 기준으로 내용 자르기
+
+let itemContentTags = document.querySelectorAll('.itemContent');
+
+
+for(const itemContentTag of itemContentTags){
+    const originData = itemContentTag.textContent;
+    itemContentTag.innerHTML = '';
+    
+    let str = '';
+    const originArr = originData.split('+');
+
+    for(const origin of originArr){
+        str += `<p>	&diams; ${origin}</p>`;
+    }
+
+    itemContentTag.insertAdjacentHTML('afterbegin', str)
+
+}
+
